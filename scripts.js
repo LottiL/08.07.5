a53c8839d066364eceed8225667b0e158dd679a8
LottiL <69212700+LottiL@users.noreply.github.com>
2020-08-14 17:56:55 +0200

    ready

---
+++ scripts.js
@@ -0,0 +1,12 @@
+let number = 0
+
+document.getElementById("incbtn").addEventListener("click", function(){
+    number++
+    $("#var").text(number)
+  });
+
+// $("#incbtn").on('click', function(){
+//     number++
+//     $("#var").text(number)
+//   });
+  
